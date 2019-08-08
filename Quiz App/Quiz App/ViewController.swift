7dba54ad15e9dd12fdb0a1b99bcd1eb1e362aa77
loredanatodoran <loredanatodoran15@gmail.com>
2019-08-08 14:14:09 +0300

    Initial commit

---
+++ Quiz App/Quiz App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by Loredana Todoran on 08/08/2019.
+//  Copyright © 2019 Loredana Todoran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
